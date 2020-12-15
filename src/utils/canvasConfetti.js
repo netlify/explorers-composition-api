56662a61f33ddc8ac14aaec142d6f7b8e714a204
Ben Hong <ben@bencodezen.io>
2020-12-15 08:18:02 -0500

    fix: confetti timing

--- src/utils/canvasConfetti.js
+++ src/utils/canvasConfetti.js
@@ -1,7 +1,7 @@
 import confetti from 'canvas-confetti'
 
 export const launchConfetti = () => {
-  var duration = 15 * 1000
+  var duration = 5000
   var animationEnd = Date.now() + duration
   var skew = 1
 
@@ -9,26 +9,30 @@ export const launchConfetti = () => {
     return Math.random() * (max - min) + min
   }
 
-  var timeLeft = animationEnd - Date.now()
-  var ticks = Math.max(200, 500 * (timeLeft / duration))
-  skew = Math.max(0.8, skew - 0.001)
+  const frame = () => {
+    var timeLeft = animationEnd - Date.now()
+    var ticks = Math.max(200, 500 * (timeLeft / duration))
+    skew = Math.max(0.8, skew - 0.001)
 
-  confetti({
-    particleCount: 1,
-    startVelocity: 0,
-    ticks: ticks,
-    gravity: 0.5,
-    origin: {
-      x: Math.random(),
-      // since particles fall down, skew start toward the top
-      y: Math.random() * skew - 0.2
-    },
-    colors: ['#ffffff'],
-    shapes: ['circle'],
-    scalar: randomInRange(0.4, 1)
-  })
+    confetti({
+      particleCount: 1,
+      startVelocity: 0,
+      ticks: ticks,
+      gravity: 0.5,
+      origin: {
+        x: Math.random(),
+        // since particles fall down, skew start toward the top
+        y: Math.random() * skew - 0.2
+      },
+      colors: ['#ffffff'],
+      shapes: ['circle'],
+      scalar: randomInRange(0.4, 1)
+    })
 
-  if (timeLeft > 0) {
-    requestAnimationFrame(launchConfetti)
+    if (timeLeft > 0) {
+      requestAnimationFrame(frame)
+    }
   }
+
+  frame()
 }
