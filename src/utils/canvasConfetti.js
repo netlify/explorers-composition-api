ec368429db77dcf40998bbb7bf0c8dec198b8cec
Ben Hong <ben@bencodezen.io>
2020-12-15 07:54:10 -0500

    feature: update animation to more space like confetti

--- src/utils/canvasConfetti.js
+++ src/utils/canvasConfetti.js
@@ -1,34 +1,34 @@
 import confetti from 'canvas-confetti'
 
 export const launchConfetti = () => {
-  const duration = 15 * 1000
-  const animationEnd = Date.now() + duration
-  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 }
+  var duration = 15 * 1000
+  var animationEnd = Date.now() + duration
+  var skew = 1
 
   function randomInRange(min, max) {
     return Math.random() * (max - min) + min
   }
 
-  const confettiInterval = setInterval(function() {
-    const timeLeft = animationEnd - Date.now()
+  var timeLeft = animationEnd - Date.now()
+  var ticks = Math.max(200, 500 * (timeLeft / duration))
+  skew = Math.max(0.8, skew - 0.001)
 
-    if (timeLeft <= 0) {
-      return clearInterval(confettiInterval)
-    }
+  confetti({
+    particleCount: 1,
+    startVelocity: 0,
+    ticks: ticks,
+    gravity: 0.5,
+    origin: {
+      x: Math.random(),
+      // since particles fall down, skew start toward the top
+      y: Math.random() * skew - 0.2
+    },
+    colors: ['#ffffff'],
+    shapes: ['circle'],
+    scalar: randomInRange(0.4, 1)
+  })
 
-    const particleCount = 50 * (timeLeft / duration)
-    // since particles fall down, start a bit higher than random
-    confetti(
-      Object.assign({}, defaults, {
-        particleCount,
-        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }
-      })
-    )
-    confetti(
-      Object.assign({}, defaults, {
-        particleCount,
-        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }
-      })
-    )
-  }, 250)
+  if (timeLeft > 0) {
+    requestAnimationFrame(launchConfetti)
+  }
 }
